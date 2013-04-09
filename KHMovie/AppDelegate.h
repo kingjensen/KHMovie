1707223aa69bd039d9a421fb2dc2f31c9881fcca
ibokan <ibokan@ibokan023.local>
2013-04-09 11:22:37 +0800

    Initial Commit

---
+++ KHMovie/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KHMovie
+//
+//  Created by ibokan on 13-4-9.
+//  Copyright (c) 2013年 ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
